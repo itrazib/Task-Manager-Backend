dc0e4cc82f92254916ce26e613d9e111a23247bf
Razib Das <it.razib.mbstu@gmail.com>
2025-12-14 20:38:28 +0600

    .

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = exports.db = void 0;
-require('dotenv').config();
 const mongodb_1 = require("mongodb");
 const client = new mongodb_1.MongoClient(process.env.MONGO_URI);
 exports.db = client.db("collaborative_task_manager");
--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/server.
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const http_1 = __importDefault(require("http"));
 const app_1 = __importDefault(require("./app"));
 const db_1 = require("./config/db");
